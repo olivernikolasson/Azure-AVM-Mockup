674ad66aec045ad6f27709333883eeeb118cb149
oliver nikolasson <oliver.nikolasson@outlook.com>
2025-02-19 14:13:46 +0100

    added tags

--- locals.tf
+++ locals.tf
@@ -1,4 +1,15 @@
+#Locals 
 locals {
   rg-web-prod = "rg-web-prod-01"
   default-region = "West Europe"
+}
+
+#Tags
+locals {
+  defaultTags = {
+    Enviroment = "Production"
+    Owner = "Oliver"
+    Backup = "30 days"
+    Country = "Sweden"
+  } 
 }
--- main.tf
+++ main.tf
@@ -3,8 +3,5 @@ resource "azurerm_resource_group" "rg-web-prod-01" {
     name = local.rg-web-prod
     location = local.default-region
 
-    tags = {
-      test = "testtag"
-    }
-  
+    tags = local.defaultTags
 }
