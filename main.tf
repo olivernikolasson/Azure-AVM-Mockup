26c004e5ef4f357f1458667760e85db2e97d1dfc
oliver nikolasson <oliver.nikolasson@outlook.com>
2025-02-19 12:44:31 +0100

    fixed stuff

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+
 resource "azurerm_resource_group" "rg-web-prod-01" {
     name = local.rg-web-prod
     location = local.default-region
--- terraform.tf
+++ terraform.tf
@@ -1,4 +1,10 @@
 terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.19.0"
+    }
+  }
   backend "azurerm" {
     resource_group_name  = "tf-state-rg"
     storage_account_name = "stateolnik"
